e1847f41a41028c343c5b15790a8d25e6b49fab9
unknown <juan.gomez.q@gmail.com>
2021-01-24 12:30:40 -0500

    Add localhost as a valid domain

--- akim/settings.py
+++ akim/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG')
 
-ALLOWED_HOSTS = ['phinxk.com', 'akim.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['phinxk.com', 'akim.herokuapp.com', '127.0.0.1', 'localhost']
 
 
 # Application definition
